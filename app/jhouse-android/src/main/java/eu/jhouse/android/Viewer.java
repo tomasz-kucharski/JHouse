650160ede387ca2856ff9f9ba58ce23b6ad0f9d4
Tom Kucharski <kucharski.tom@gmail.com>
2011-12-15 00:46:00 +0100

    #5 removed j2me and android stuff

--- app/jhouse-android/src/main/java/eu/jhouse/android/Viewer.java
+++
@@ -1,8 +0,0 @@
-package eu.jhouse.android;
-
-/**
- * @author tomekk
- * @since 2010-08-23, 00:26:03
- */
-public class Viewer {
-}
--- app/jhouse-api/src/main/java/pl/jhouse/Connector.java
+++
@@ -1,9 +0,0 @@
-package pl.jhouse;
-
-/**
- * @author tomekk
- * @since 2010-10-17, 01:07:58
- */
-public interface Connector {
-    public String hello(String message);
-}
