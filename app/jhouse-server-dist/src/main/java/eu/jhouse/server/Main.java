d81a5c148a3f77dd9a666be04988c0a411335b99
Tom Kucharski <kucharski.tom@gmail.com>
2011-12-16 01:47:53 +0100

    #5 isolated artifact jhouse-server-dist for embedded jetty server distribution

--- app/jhouse-server-dist/src/main/java/eu/jhouse/server/Main.java
+++ app/jhouse-server-dist/src/main/java/eu/jhouse/server/Main.java
@@ -1,5 +1,9 @@
 package eu.jhouse.server;
 
+import java.net.URL;
+import java.security.ProtectionDomain;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -7,17 +11,27 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @since 2010-10-06, 01:07:42
  */
 public class Main {
+	private static Logger log = LoggerFactory.getLogger(Main.class);
 
 	public static final String DEFAULT_JETTY_CONFIGURATION_PATH = "jetty.xml";
 
 	public static void main(String args[]) throws Exception {
-        String springPath;
-        if (args.length != 0) {
-            springPath = args[0];
-        } else {
-            springPath = DEFAULT_JETTY_CONFIGURATION_PATH;
-        }
-		new ClassPathXmlApplicationContext(springPath);
-    }
+		String springPath;
+		if (args.length != 0) {
+			springPath = args[0];
+		} else {
+			springPath = DEFAULT_JETTY_CONFIGURATION_PATH;
+		}
+		log.debug("Starting JHouse Jetty server using:"+springPath);
+		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(springPath);
+		log.debug("JHouse Jetty server started");
+
+	}
+
+	public static String getWEBINFfileLocation() {
+		ProtectionDomain protectionDomain = Main.class.getProtectionDomain();
+		URL location = protectionDomain.getCodeSource().getLocation();
+		return location.toExternalForm();
+	}
 
 }
